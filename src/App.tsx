a8d8294640d186a0fa14cc3f4e153020d238b4e6
Daniil <verkhovskyi.daniil@gmail.com>
2025-12-28 18:25:54 +0100

    Initial Vite + Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="text-2xl font-bold">Crypto Tracker App</h1>
+      <Button variant="destructive" size="lg">
+        Click Me
+      </Button>
+    </div>
+  )
+}
+export default App
